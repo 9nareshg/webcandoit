cd9f9b00e83709de68b1fb1a63038e6bf45cf678
9naresh <32484056+9naresh@users.noreply.github.com>
2019-12-15 22:07:00 +0530

    header designed
    Signed-off-by: 9naresh <32484056+9naresh@users.noreply.github.com>

---
+++ include-file/header.php
@@ -0,0 +1,93 @@
+<header>
+	<div class="page-base">
+		<span class="bar">
+			<div></div>
+			<div></div>
+			<div></div>
+		</span>
+		<ul class="main-nav" style="float: left;margin-left: 90px;">
+			<li class="active"><a href="#">HOME</a></li>
+			<li><a href="#">WEB DESIGN SERVICES</a></li>
+		</ul>
+		<div class="logo">
+			<img src="img/logo.jpg">
+			<img src="img/logo-2x.jpg">
+		</div>
+		<ul class="main-nav" style="float: right;margin-right: 135px;">
+			<li><a href="#">PRICING</a></li>		
+			<li><a href="#">PORTFOLIO</a></li>
+		</ul>
+		<ul class="account">
+			<li><a href="#">Login</a></li>
+			<li>|</li>
+			<li><a href="#">Register</a></li>
+		</ul>
+	</div>
+	<nav>
+		<div class="page-base">
+			<ul>
+				<li><a href="#">Restaurant</a>
+					<ul>
+						<li></li>
+					</ul>
+				</li>
+				<li><a href="#">School & College</a>
+					<ul>
+						<li></li>
+					</ul>	
+				</li>
+				<li><a href="#">Showroom Site</a>
+					<ul>
+						<li></li>
+					</ul>
+				</li>
+				<li><a href="#">Real State</a>
+					<ul>
+						<li></li>
+					</ul>
+				</li>
+				<li><a href="#">Gym</a>
+					<ul>
+						<li></li>
+					</ul>
+				</li>
+				<li><a href="#">Yoga</a>
+					<ul>
+						<li></li>
+					</ul>
+				</li>
+				<li><a href="#">Ngo</a>
+					<ul>
+						<li></li>
+					</ul>
+				</li>
+				<li><a href="#">Travel</a>
+					<ul>
+						<li></li>
+					</ul>
+				</li>
+				<li><a href="#">Online store</a>
+					<ul>
+						<li></li>
+					</ul>
+				</li>
+				<li><a href="#">Finanace & Law</a>
+					<ul>
+						<li></li>
+					</ul>
+				</li>
+				<li><a href="#">All Types</a>
+					<ul>
+						<li></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+	</nav>
+	<ul class="mobile-nav">
+		<li>Home</li>
+		<li>Web Design services</li>
+		<li>pricing</li>
+		<li>Portfolio</li>
+	</ul>
+</header>
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>HOME</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/pages/index.css">
+</head>
+<body>
+	<!-- svg sprites -->
+	<?php include("img/sprites-icon/sprite.svg") ?>
+	
+	<!-- header -->
+	<?php include("include-file/header.php") ?>
+
+	<!-- home page -->
+	<div class="content">
+		<div class="page-base">
+		</div>
+	</div>
+
+	<!-- footer -->
+	<?php include("include-file/footer.php") ?>
+
+	<!-- jquery script -->
+	<script src="js/common.js"></script>
+</body>
+</html>
